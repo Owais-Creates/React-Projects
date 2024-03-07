e0b1fbeb774ddf435fe3746c3344abf150f48e03
Owais <zaidalit00@gmail.com>
2024-03-07 15:30:00 +0530

    Navbar

---
+++ nike/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+
+                <div className="image">
+                    <img src="/images/brand_logo.png" alt="" />
+                </div>
+
+                    <ul className="list-items">
+                        <li><a href="">MENU</a></li>
+                        <li><a href="">LOCATION</a></li>
+                        <li><a href="">ABOUT</a></li>
+                        <li><a href="">CONTACT</a></li>
+                        
+                    </ul>
+    
+                <button className='login-btn'>Login</button>
+
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
